23f57eb1e2ce50c200ccf1178c3187f542830ab3
dora <dorahud@163.com>
2017-12-29 17:00:59 +0800

    maventest

---
+++ itcast_test/src/main/java/com/dora/itcast_test/App.java
@@ -0,0 +1,13 @@
+package com.dora.itcast_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
